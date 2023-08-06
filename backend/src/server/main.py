21b224ef6c01a00c868bb353b418fa3641d3c3d5
DylanLoram <lrmdyl001@myuct.ac.za>
2023-08-06 11:57:00 +0200

    created index.py

--- backend/src/server/main.py
+++ backend/src/server/main.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from server import config_view, display_view
+from server import config_view, display_view, index
 
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(config_view.blueprint)
     app.register_blueprint(display_view.blueprint)
+    app.register_blueprint(index.blueprint)
     return app
 
 
