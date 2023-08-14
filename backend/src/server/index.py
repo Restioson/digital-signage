f9a3e9654a323fe9cbd4ea40889965766398598c
DylanLoram <lrmdyl001@myuct.ac.za>
2023-08-14 12:13:27 +0200

    fixed the format error with the header

--- backend/src/server/index.py
+++ backend/src/server/index.py
@@ -6,5 +6,5 @@ blueprint = Blueprint("index", __name__, url_prefix="/")
 @blueprint.route("/")
 def index():
     return """<p>Welcome to campusign</p>
-            <p> <a href="/config">link to the configuration view</a> </p>
+            <p><a href="/config/department/lecturer" >here is config view</a></p>
             <p> <a href="/display">link to the display view</a> </p>"""
