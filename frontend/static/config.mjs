8c3dae1a7134583c64a977841477e4a742bb7b4d
Restioson <restiosondev@gmail.com>
2023-08-16 11:53:54 +0200

    Provide feedback upon post form submission
    This is just an MVP but doesn't show it very nicely

---
+++ frontend/static/config.mjs
@@ -0,0 +1,57 @@
+export function main () {
+  for (const form of document.getElementsByClassName('post_form')) {
+    console.log('Added listener')
+    form.addEventListener('submit', submitPost)
+  }
+}
+
+class ApiError extends Error {
+  constructor (response) {
+    super(`Response from API: ${response}`)
+    this.name = 'ApiError'
+    this.response = response
+  }
+}
+
+/**
+ * Submit a 'create post' form without redirecting the page
+ *
+ * @param event the HTML form submit event
+ * @returns {Promise<void>}
+ */
+async function submitPost (event) {
+  event.preventDefault()
+  const form = event.target
+
+  const postStatusMessage = form.querySelector('.post-status-message')
+  postStatusMessage.className = 'post-status-message' // Clear success/error class
+
+  try {
+    const res = await fetch(form.action, {
+      method: 'post',
+      headers: { 'Content-Type': form.enctype },
+      // FormData always encodes as multipart/form-data so urlencoded data needs to be converted
+      body:
+        form.enctype === 'multipart/form-data'
+          ? new FormData(form)
+          : new URLSearchParams(new FormData(form))
+    })
+
+    if (res.status !== 200) {
+      throw new ApiError(await res.text())
+    }
+
+    postStatusMessage.classList.add('success')
+    postStatusMessage.innerText = `Successfully submitted post (id: ${
+      (await res.json()).id
+    })`
+  } catch (err) {
+    postStatusMessage.classList.add('error')
+    const errorBox = document.createElement('pre')
+    errorBox.innerText = err instanceof ApiError ? err.response : err.message
+    postStatusMessage.innerText = 'Error submitting post:'
+    postStatusMessage.append(errorBox)
+  }
+
+  postStatusMessage.hidden = false
+}
