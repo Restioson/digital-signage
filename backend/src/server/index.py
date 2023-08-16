5f405f3c65675baa23afc489ad57255a13bc41b0
Restioson <restiosondev@gmail.com>
2023-08-16 12:24:05 +0200

    Create Jinja2 template for index page

--- backend/src/server/index.py
+++ backend/src/server/index.py
@@ -1,10 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 blueprint = Blueprint("index", __name__, url_prefix="/")
 
 
 @blueprint.route("/")
 def index():
-    return """<p>Welcome to campusign</p>
-            <p><a href="/config/department/lecturer" >here is config view</a></p>
-            <p> <a href="/display">link to the display view</a> </p>"""
+    return render_template("index.j2")
