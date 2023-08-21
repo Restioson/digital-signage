ca620a12430fe46f267b420fa7a73cc43598ccdd
Restioson <restiosondev@gmail.com>
2023-08-21 18:41:04 +0200

    feat: Lecturer widget

--- frontend/static/display.mjs
+++ frontend/static/display.mjs
@@ -1,23 +1,7 @@
 import { deserializeFreeFormContent } from './widgets/free_form_content/free_form_content_factory.mjs'
+import { Lecturer } from './widgets/lecturer.js'
 
 const REFRESH_INTERVAL_MS = 1000
-
-export function renderLecturer (lecturer) {
-  const container = document.createElement('div')
-  const title = document.createElement('h3')
-  const body = document.createElement('p')
-
-  title.innerText = `${lecturer.title} ${lecturer.name}`
-  body.innerText = `Position: ${lecturer.position} in the ${
-    lecturer.department
-  } department\n
-  Office Hours: ${lecturer.office_hours}\nOffice Location: ${
-    lecturer.office_location
-  }\n
-  Email: ${lecturer.email}\nPhone: ${lecturer.phone}`
-  container.append(title, body)
-  return container
-}
 
 export async function refresh () {
   const contentUpdate = await fetch('/api/content').then(res => res.json())
@@ -28,7 +12,7 @@ export async function refresh () {
   lecturerContainer.innerHTML = ''
 
   for (const lecturer of lecturerUpdate.lecturers) {
-    lecturerContainer.appendChild(renderLecturer(lecturer))
+    lecturerContainer.appendChild(Lecturer.fromJSON(lecturer).render())
   }
   for (const content of contentUpdate.content) {
     contentContainer.appendChild(deserializeFreeFormContent(content).render())
---
+++ frontend/static/widgets/lecturer.js
@@ -0,0 +1,45 @@
+import { Widget } from '../widget.mjs'
+
+export class Lecturer extends Widget {
+  constructor ({ department, position, title, name, officeHours, officeLocation, email, phone }) {
+    super()
+    this.department = department
+    this.position = position
+    this.title = title
+    this.name = name
+    this.officeHours = officeHours
+    this.officeLocation = officeLocation
+    this.email = email
+    this.phone = phone
+  }
+
+  static fromJSON (obj) {
+    return new Lecturer({
+      department: obj.department,
+      position: obj.position,
+      title: obj.title,
+      name: obj.name,
+      officeHours: obj.office_hours,
+      officeLocation: obj.office_location,
+      email: obj.email,
+      phone: obj.phone
+    })
+  }
+
+  render () {
+    const container = document.createElement('div')
+    const title = document.createElement('h3')
+    const body = document.createElement('p')
+
+    title.innerText = `${this.title} ${this.name}`
+    body.innerText =
+      `Position: ${this.position} in the ${this.department} department\n` +
+      `Office Hours: ${this.officeHours}\n` +
+      `Office Location: ${this.officeLocation}\n` +
+      `Email: ${this.email}\n` +
+      `Phone: ${this.phone}`
+
+    container.append(title, body)
+    return container
+  }
+}
