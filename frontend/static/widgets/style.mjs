10ee7032674ddd6d3382b84b422ef9934a1e5b25
Restioson <restiosondev@gmail.com>
2023-09-10 22:43:14 +0200

    feat: add style widget

---
+++ frontend/static/widgets/style.mjs
@@ -0,0 +1,26 @@
+import { DeserializableWidget } from './deserializable/deserializable_widget.mjs'
+
+/**
+ * A widget which displays an HTML style element.
+ */
+export class StyleWidget extends DeserializableWidget {
+  constructor ({ element }) {
+    super()
+    this.element = element
+  }
+
+  static fromXML (tag) {
+    const style = document.createElement('style')
+    style.appendChild(document.createTextNode(tag.text()))
+
+    for (const attr of tag.attributes) {
+      style[attr] = tag[attr]
+    }
+
+    return new StyleWidget({ element: style })
+  }
+
+  build () {
+    return this.element
+  }
+}
