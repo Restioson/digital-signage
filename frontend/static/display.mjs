e14e785e543026dcb2c86dde32e80301f2e97d39
Restioson <restiosondev@gmail.com>
2023-09-12 11:04:08 +0200

    feat: preview display group
    Signed-off-by: Restioson <restiosondev@gmail.com>

--- frontend/static/display.mjs
+++ frontend/static/display.mjs
@@ -2,6 +2,14 @@ import { Root } from './widgets/root.mjs'
 import { deserializeWidgetFromXML } from './widgets/deserializable/widget_deserialization_factory.mjs'
 
 export function main ({ department, layout }) {
+  window.addEventListener('error', function (error) {
+    const div = document.getElementById('errors')
+    div.hidden = false
+    const pre = document.createElement('pre')
+    pre.innerText = error.message
+    div.appendChild(pre)
+  })
+
   Root.create({
     child: deserializeWidgetFromXML(layout),
     targetElement: document.getElementById('root'),
