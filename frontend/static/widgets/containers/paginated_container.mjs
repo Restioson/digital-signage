e5a4f9733c3ead320635b4d4a613db33e605096f
Restioson <restiosondev@gmail.com>
2023-09-12 12:08:25 +0200

    feat: add ability to paginate department/content stream
    Signed-off-by: Restioson <restiosondev@gmail.com>

---
+++ frontend/static/widgets/containers/paginated_container.mjs
@@ -0,0 +1,28 @@
+import { Widget } from '../widget.mjs'
+import { Container } from './container.mjs'
+
+/**
+ * A paginated container is like a container, but it shows only `pageSize` children at a time.
+ *
+ * The `page` attribute will automatically be wrapped according to the total number of pages.
+ */
+export class PaginatedContainer extends Widget {
+  constructor ({ children, pageSize, page }) {
+    super()
+    this.children = children
+    this.pageSize = pageSize
+    this.page = page % Math.ceil(this.children.length / pageSize)
+  }
+
+  build () {
+    const start = this.page * this.pageSize
+    const end = start + this.pageSize
+    return new Container({
+      children: this.children.slice(start, end)
+    })
+  }
+
+  className () {
+    return 'paginated-container'
+  }
+}
--- frontend/static/widgets/free_form_content/content_stream.mjs
+++ frontend/static/widgets/free_form_content/content_stream.mjs
@@ -2,6 +2,7 @@ import { WithRefresh } from '../dynamic/with_refresh.mjs'
 import { DeserializableWidget } from '../deserializable/deserializable_widget.mjs'
 import { Container } from '../containers/container.mjs'
 import { deserializeFreeFormContent } from './free_form_content_factory.mjs'
+import { PaginatedContainer } from '../containers/paginated_container.mjs'
 
 const REFRESH_INTERVAL_MS = 1000
 
@@ -15,11 +16,14 @@ export class ContentStream extends DeserializableWidget {
    * @param {number[]} streams which content streams this widget subscribes to
    * @param {?number} the amount of posts to fetch
    */
-  constructor ({ fetchAmount, streams }) {
+  constructor ({ fetchAmount, streams, pageSize, rotateEveryNSec }) {
     super()
     this.children = []
     this.fetchAmount = fetchAmount
     this.streams = streams
+    this.pageSize = parseInt(pageSize)
+    this.page = 0
+    this.rotationPeriod = (rotateEveryNSec || 10) * 1000
   }
 
   /**
@@ -59,7 +63,9 @@ export class ContentStream extends DeserializableWidget {
   static fromXML (tag) {
     return new ContentStream({
       fetchAmount: tag.attribute('fetch-amount'),
-      streams: tag.children().map(stream => parseInt(stream.attribute('id')))
+      streams: tag.children().map(stream => parseInt(stream.attribute('id'))),
+      pageSize: tag.attribute('page-size'),
+      rotateEveryNSec: tag.attribute('secs-per-page')
     })
   }
 
@@ -67,7 +73,26 @@ export class ContentStream extends DeserializableWidget {
     return new WithRefresh({
       refresh: () => this.refresh(),
       period: REFRESH_INTERVAL_MS,
-      builder: () => new Container({ children: this.children })
+      builder: () => {
+        if (this.pageSize) {
+          return new WithRefresh({
+            refresh: () => {
+              this.page += 1
+              return true
+            },
+            period: this.rotationPeriod,
+            builder: () => {
+              return new PaginatedContainer({
+                children: this.children,
+                pageSize: this.pageSize,
+                page: this.page
+              })
+            }
+          })
+        } else {
+          return new Container({ children: this.children })
+        }
+      }
     })
   }
 
