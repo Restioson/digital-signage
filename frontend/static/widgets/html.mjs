5419570b591b610a5cc4ec24a49ea5ac1bbb1421
Restioson <restiosondev@gmail.com>
2023-09-17 11:30:32 +0200

    feat: allow disabling of ShadowDom in custom HTML template
    Signed-off-by: Restioson <restiosondev@gmail.com>

--- frontend/static/widgets/html.mjs
+++ frontend/static/widgets/html.mjs
@@ -1,4 +1,5 @@
 import { DeserializableWidget } from './deserializable/deserializable_widget.mjs'
+import { Root } from './root.mjs'
 
 /**
  * A widget which displays a given HTML element.
@@ -21,6 +22,23 @@ export class HtmlWidget extends DeserializableWidget {
   }
 
   build () {
+    const scripts = this.element.querySelectorAll('script')
+    Root.getInstance().watchElement({
+      element: this.element,
+      onAdd: () => {
+        for (const script of this.element.querySelectorAll('script')) {
+          const cloned = document.createElement('script')
+
+          Array.from(script.attributes).forEach(attribute => {
+            cloned.setAttribute(attribute.name, attribute.value)
+          })
+
+          cloned.text = script.text
+          script.parentNode.replaceChild(cloned, script)
+        }
+      }
+    })
+
     return this.element
   }
 
